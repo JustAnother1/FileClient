2bdd7380e550638601a258a79c7bfe3cafd69954
Lars Pötter <kontakt@ing-poetter.de>
2022-11-17 23:24:35 +0100

    moved request handling in separate class to be able to use that class in other projects.

--- src/de/nomagic/FileClient.java
+++ src/de/nomagic/FileClient.java
@@ -1,13 +1,10 @@
 package de.nomagic;
 
-import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.Socket;
-import java.net.UnknownHostException;
+
 
 
 public class FileClient
@@ -15,16 +12,10 @@ public class FileClient
     private int ServerPort = 4321;
     private String ServerURL = "127.0.0.1";
     private String ClientId = null;
-    private String localFileName = "";
-    private String remoteFileName = "";
+    private String localFileName = null;
+    private String remoteFileName = null;
     private String command = "";
-    private boolean isConnected = false;
-    private boolean sendFile = false;
-    private boolean receiveFile = false;
-    private Socket clientSocket;
-    private InputStream inFromServer;
-    private DataOutputStream outToServer;
-    private File LocalFile = null;
+
 
     public FileClient()
     {
@@ -40,6 +31,66 @@ public class FileClient
             System.out.println("Failed !");
         }
         System.exit(res);
+    }
+
+    public int doTheWork()
+    {
+        if("".equals(command))
+        {
+            printHelpText();
+            System.exit(-1);
+        }
+        // Connect to server
+        FileInputStream fin = null;
+        FileOutputStream fout = null;
+        File LocalFile = null;
+        try
+        {
+            FileRequest fr = new FileRequest(ServerURL, ServerPort, ClientId);
+            if(null != localFileName)
+            {
+                LocalFile = new File(localFileName);
+                if((true == "store".equals(command)) || (true == "update".equals(command) || (true == "compare".equals(command))))
+                {
+                    // read in local file
+                    if(false == LocalFile.exists())
+                    {
+                        System.err.println("ERROR: file that shall be send does not exist!");
+                        return -2;
+                    }
+                    if(false == LocalFile.canRead())
+                    {
+                        System.err.println("ERROR: file that shall be send can not be read!");
+                        return -3;
+                    }
+                    fin = new FileInputStream(LocalFile);
+                    fr.addLocalFileForReading(fin, LocalFile.length());
+                }
+                else if(true == "get".equals(command))
+                {
+                    LocalFile = new File(remoteFileName);
+                    fout = new FileOutputStream(LocalFile);
+                    fr.addLocalFileForWriting(fout);
+                }
+                // else no files needed for "has",..
+            }
+            int res =  fr.makeRequest(command, remoteFileName);
+            if(null != fin)
+            {
+                fin.close();
+            }
+            if(null != fout)
+            {
+                fout.close();
+            }
+            return res;
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            System.err.println("ERROR: IOException !");
+        }
+        return -11;
     }
 
     private void printHelpText()
@@ -79,7 +130,6 @@ public class FileClient
                     localFileName = args[i];
                     i++;
                     remoteFileName = args[i];
-                    sendFile = true;
                 }
                 else if(true == "-update".equals(args[i]))
                 {
@@ -88,14 +138,12 @@ public class FileClient
                     localFileName = args[i];
                     i++;
                     remoteFileName = args[i];
-                    sendFile = true;
                 }
                 else if(true == "-get".equals(args[i]))
                 {
                     command = "get";
                     i++;
                     remoteFileName = args[i];
-                    receiveFile = true;
                 }
                 else if(true == "-has".equals(args[i]))
                 {
@@ -130,7 +178,6 @@ public class FileClient
                     localFileName = args[i];
                     i++;
                     remoteFileName = args[i];
-                    receiveFile = true;
                 }
                 else
                 {
@@ -148,325 +195,4 @@ public class FileClient
         }
     }
 
-    public int doTheWork()
-    {
-        if("".equals(command))
-        {
-            printHelpText();
-            System.exit(-1);
-        }
-        // Connect to server
-        connectTo(ServerURL, ServerPort);
-        if(false == isConnected)
-        {
-            System.err.println("Could not connect to Server !");
-            System.exit(-1);
-        }
-        int res = executeCommand();
-        disconnect();
-        return res;
-    }
-
-    private int sendRequest() throws IOException
-    {
-        // construct request
-        StringBuffer RequestHeader = new StringBuffer();
-        RequestHeader.append("2:");
-        if("compare".equals(command))
-        {
-            RequestHeader.append("get:");
-        }
-        else
-        {
-            RequestHeader.append(command + ":");
-        }
-        if(null != ClientId)
-        {
-            RequestHeader.append("name=" + ClientId + ":");
-        }
-        RequestHeader.append("file=" + remoteFileName + ":");
-        if(true == sendFile)
-        {
-            LocalFile = new File(localFileName);
-            if(false == LocalFile.exists())
-            {
-                System.err.println("ERROR: file that shall be send does not exist!");
-                return -2;
-            }
-            if(false == LocalFile.canRead())
-            {
-                System.err.println("ERROR: file that shall be send can not be read!");
-                return -3;
-            }
-            RequestHeader.append("fileContentLength=" + LocalFile.length() + ":");
-        }
-        String requestHeaderString = RequestHeader.toString() + "\n";
-        System.out.println("Sending Request: " + requestHeaderString);
-        outToServer.writeBytes(requestHeaderString);
-        // send file data if we have to
-        if(true == sendFile)
-        {
-            byte[] buf = new byte[4096];
-            FileInputStream fin = new FileInputStream(LocalFile);
-            int num;
-            do {
-                num = fin.read(buf);
-                if(num > 0)
-                {
-                    outToServer.write(buf, 0, num);
-                }
-            }while(num > 0);
-            fin.close();
-            outToServer.writeBytes("2:\n");
-        }
-        outToServer.flush();
-        return 0;
-    }
-
-    private int handleReceiveFile(String[] sections) throws IOException
-    {
-        boolean identical = true;
-        // get file length
-        long lengthOfFile = 0;
-        for(int i = 0; i < sections.length; i++)
-        {
-            if(true == sections[i].startsWith("fileContentLength"))
-            {
-                String fs = sections[i].substring(sections[i].indexOf('=') + 1);
-                fs = fs.trim();
-                lengthOfFile = Long.parseLong(fs);
-            }
-        }
-        if(0 == lengthOfFile)
-        {
-            System.err.println("ERROR: no file length provided by server !");
-            return -12;
-        }
-
-        if("compare".equals(command))
-        {
-            LocalFile = new File(localFileName);
-            if(false == LocalFile.exists())
-            {
-                System.err.println("ERROR: local file does not exist!");
-                return -2;
-            }
-            if(false == LocalFile.canRead())
-            {
-                System.err.println("ERROR: local file can not be read!");
-                return -3;
-            }
-            if(lengthOfFile != LocalFile.length())
-            {
-                System.err.println("COMAPRE: different file sizes !");
-                identical = false;
-            }
-
-            byte[] remoteBuf = new byte[4096];
-            byte[] localBuf = new byte[4096];
-            boolean reported = false;
-            FileInputStream fin = new FileInputStream(LocalFile);
-            int num;
-            do {
-                int maxLength = 4096;
-                if(lengthOfFile < maxLength)
-                {
-                    maxLength = (int)lengthOfFile;
-                }
-                num = inFromServer.read(remoteBuf, 0, maxLength);
-                if(num > 0)
-                {
-                    if(num != fin.read(localBuf, 0, num))
-                    {
-                        System.err.println("ERROR: local file can not be read!");
-                        fin.close();
-                        return -3;
-                    }
-                    for(int i = 0; i < num; i++)
-                    {
-                        if(localBuf[i] != remoteBuf[i])
-                        {
-                            if(false == reported)
-                            {
-                                System.err.println("COMAPRE: different file content !");
-                                reported = true;
-                            }
-                            identical = false;
-                            break;
-                        }
-                    }
-                    lengthOfFile = lengthOfFile - num;
-                }
-            }while(num > 0);
-            fin.close();
-        }
-        else
-        {
-            // save the file locally
-            LocalFile = new File(remoteFileName);
-            FileOutputStream fout = new FileOutputStream(LocalFile);
-            byte[] buf = new byte[4096];
-            int num;
-            do {
-                int maxLength = 4096;
-                if(lengthOfFile < maxLength)
-                {
-                    maxLength = (int)lengthOfFile;
-                }
-                num = inFromServer.read(buf, 0, maxLength);
-                // System.out.println("bytes to go: " + lengthOfFile + ", max chunk : "  + maxLength + ", actually read: "  + num);
-                if(0 < num)
-                {
-                    fout.write(buf, 0, num);
-                    lengthOfFile = lengthOfFile - num;
-                }
-            }while((num > 0) && (lengthOfFile > 0));
-            fout.close();
-        }
-
-        // read end of reply
-        int b = inFromServer.read();
-        if('2' != b)
-        {
-            System.err.println("ERROR: reply invalid (4) : " + (char) b + "!");
-            b = inFromServer.read();
-            System.err.println(" " + (char) b );
-            b = inFromServer.read();
-            System.err.println(" " + (char) b );
-            b = inFromServer.read();
-            System.err.println(" " + (char) b );
-            return -8;
-        }
-        b = inFromServer.read();
-        if(':' != b)
-        {
-            System.err.println("ERROR: reply invalid (5)!");
-            return -9;
-        }
-        b = inFromServer.read();
-        if('\n' != b)
-        {
-            System.err.println("ERROR: reply invalid (6)!");
-            return -10;
-        }
-        if("compare".equals(command))
-        {
-            if(true == identical)
-            {
-                return 0;
-            }
-            else
-            {
-                return 1;
-            }
-        }
-        return 0;
-    }
-
-    public int executeCommand()
-    {
-        try
-        {
-            int res = sendRequest();
-            if(0 != res)
-            {
-                // sending the request did not work
-                return res;
-            }
-            // Request has been send to the server
-
-            // Parse reply
-            int b = inFromServer.read();
-            if('2' != b)
-            {
-                System.err.println("ERROR: reply invalid (1)!");
-                return -4;
-            }
-            b = inFromServer.read();
-            if(':' != b)
-            {
-                System.err.println("ERROR: reply invalid (2)!");
-                return -5;
-            }
-            b = inFromServer.read();
-            if('0' != b)
-            {
-                System.out.println("Error state " + (char)b + " received from Server!");
-                int err = b;
-                StringBuffer Reason = new StringBuffer();
-                try
-                {
-                    do {
-                        b = inFromServer.read();
-                        Reason.append((char)b);
-                    }while('\n' != b);
-                }
-                catch (IOException e)
-                {
-                    isConnected = false;
-                    e.printStackTrace();
-                    System.err.println("ERROR: IOException !");
-                }
-                System.out.println("Reason: " + Reason.toString());
-                return err;
-            }
-            b = inFromServer.read();
-            if(':' != b)
-            {
-                System.err.println("ERROR: reply invalid (3)!");
-                return -7;
-            }
-            StringBuffer DataSections = new StringBuffer();
-            do {
-                b = inFromServer.read();
-                DataSections.append((char)b);
-            }while('\n' != b);
-            String[] sections = DataSections.toString().split(":");
-            if(true == receiveFile)
-            {
-                res = handleReceiveFile(sections);
-            }
-            return res;
-        }
-        catch (IOException e)
-        {
-            isConnected = false;
-            e.printStackTrace();
-            System.err.println("ERROR: IOException !");
-        }
-        System.err.println("ERROR: reply invalid (7)!");
-        return -11;
-    }
-
-    public void connectTo(String serverURL, int serverPort)
-    {
-        try
-        {
-            clientSocket = new Socket(serverURL, serverPort);
-            outToServer = new DataOutputStream(clientSocket.getOutputStream());
-            inFromServer = clientSocket.getInputStream();
-            isConnected = true;
-        }
-        catch (UnknownHostException e)
-        {
-            e.printStackTrace();
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-    }
-
-    public void disconnect()
-    {
-        try
-        {
-            clientSocket.close();
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-        isConnected = false;
-    }
 }
---
+++ src/de/nomagic/FileRequest.java
@@ -0,0 +1,361 @@
+package de.nomagic;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class FileRequest
+{
+    public final static int NOT_IDENTICAL = 1;
+    public final static int OK = 0;
+    public final static int ERROR_CONNECTION_SERVER_FAILED = -1;
+    public final static int ERROR_SERVER_NO_FILE_LENGTH = -2;
+    public final static int ERROR_LOCAL_READ_FAILED = -3;
+    public final static int ERROR_SERVER_INVALID_REPLY = -4;
+    public final static int ERROR_USER_INVALID_COMMAND = -5;
+    public final static int ERR_REPORTED_SERVER = -10; // must always be lowest value
+
+    private Socket clientSocket;
+    private InputStream inFromServer;
+    private DataOutputStream outToServer;
+
+    private String serverURL;
+    private int serverPort;
+    private String clientId;
+
+    private InputStream localFileRead = null;
+    private long localFileLength = 0;
+    private OutputStream localFileWrite = null;
+
+    public FileRequest(String serverURL, int serverPort, String clientId)
+    {
+        this.serverURL = serverURL;
+        this.serverPort = serverPort;
+        this.clientId = clientId;
+    }
+
+    public void addLocalFileForReading(InputStream fin, long length)
+    {
+        localFileRead = fin;
+        localFileLength = length;
+    }
+
+    public void addLocalFileForWriting(OutputStream fout)
+    {
+        localFileWrite = fout;
+    }
+
+    public int makeRequest(String command, String remoteFileName)
+    {
+        if(false == connectTo(serverURL, serverPort))
+        {
+            System.err.println("Could not connect to Server !");
+            return ERROR_CONNECTION_SERVER_FAILED;
+        }
+        int res = executeCommand(command, remoteFileName);
+        disconnect();
+        return res;
+    }
+
+    private boolean connectTo(String serverURL, int serverPort)
+    {
+        try
+        {
+            clientSocket = new Socket(serverURL, serverPort);
+            outToServer = new DataOutputStream(clientSocket.getOutputStream());
+            inFromServer = clientSocket.getInputStream();
+            return true;
+        }
+        catch (UnknownHostException e)
+        {
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    private void disconnect()
+    {
+        try
+        {
+            clientSocket.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private int sendRequest(String command, String remoteFileName, boolean sendFile) throws IOException
+    {
+        // construct request
+        StringBuffer RequestHeader = new StringBuffer();
+        RequestHeader.append("2:");
+        if("compare".equals(command))
+        {
+            RequestHeader.append("get:");
+        }
+        else
+        {
+            RequestHeader.append(command + ":");
+        }
+        if(null != clientId)
+        {
+            RequestHeader.append("name=" + clientId + ":");
+        }
+        RequestHeader.append("file=" + remoteFileName + ":");
+        if(true == sendFile)
+        {
+            RequestHeader.append("fileContentLength=" + localFileLength + ":");
+        }
+        String requestHeaderString = RequestHeader.toString() + "\n";
+        System.out.println("Sending Request: " + requestHeaderString);
+        outToServer.writeBytes(requestHeaderString);
+        // send file data if we have to
+        if(true == sendFile)
+        {
+            byte[] buf = new byte[4096];
+            int num;
+            do {
+                num = localFileRead.read(buf);
+                if(num > 0)
+                {
+                    outToServer.write(buf, 0, num);
+                }
+            }while(num > 0);
+            outToServer.writeBytes("2:\n");
+        }
+        outToServer.flush();
+        return OK;
+    }
+
+    private int handleReceiveFile(String[] sections, String command) throws IOException
+    {
+        boolean identical = true;
+        // get file length
+        long lengthOfFile = 0;
+        for(int i = 0; i < sections.length; i++)
+        {
+            if(true == sections[i].startsWith("fileContentLength"))
+            {
+                String fs = sections[i].substring(sections[i].indexOf('=') + 1);
+                fs = fs.trim();
+                lengthOfFile = Long.parseLong(fs);
+            }
+        }
+        if(0 == lengthOfFile)
+        {
+            System.err.println("ERROR: no file length provided by server !");
+            return ERROR_SERVER_NO_FILE_LENGTH;
+        }
+
+        if("compare".equals(command))
+        {
+            if(lengthOfFile != localFileLength)
+            {
+                System.err.println("COMAPRE: different file sizes !");
+                identical = false;
+            }
+
+            byte[] remoteBuf = new byte[4096];
+            byte[] localBuf = new byte[4096];
+            boolean reported = false;
+            int num;
+            do {
+                int maxLength = 4096;
+                if(lengthOfFile < maxLength)
+                {
+                    maxLength = (int)lengthOfFile;
+                }
+                num = inFromServer.read(remoteBuf, 0, maxLength);
+                if(num > 0)
+                {
+                    if(num != localFileRead.read(localBuf, 0, num))
+                    {
+                        System.err.println("ERROR: local file can not be read!");
+                        return ERROR_LOCAL_READ_FAILED;
+                    }
+                    for(int i = 0; i < num; i++)
+                    {
+                        if(localBuf[i] != remoteBuf[i])
+                        {
+                            if(false == reported)
+                            {
+                                System.err.println("COMAPRE: different file content !");
+                                reported = true;
+                            }
+                            identical = false;
+                            break;
+                        }
+                    }
+                    lengthOfFile = lengthOfFile - num;
+                }
+            }while(num > 0);
+        }
+        else
+        {
+            // save the file locally
+            byte[] buf = new byte[4096];
+            int num;
+            do {
+                int maxLength = 4096;
+                if(lengthOfFile < maxLength)
+                {
+                    maxLength = (int)lengthOfFile;
+                }
+                num = inFromServer.read(buf, 0, maxLength);
+                // System.out.println("bytes to go: " + lengthOfFile + ", max chunk : "  + maxLength + ", actually read: "  + num);
+                if(0 < num)
+                {
+                    localFileWrite.write(buf, 0, num);
+                    lengthOfFile = lengthOfFile - num;
+                }
+            }while((num > 0) && (lengthOfFile > 0));
+        }
+
+        // read end of reply
+        int b = inFromServer.read();
+        if('2' != b)
+        {
+            System.err.println("ERROR: reply invalid (4) : " + (char) b + "!");
+            b = inFromServer.read();
+            System.err.println(" " + (char) b );
+            b = inFromServer.read();
+            System.err.println(" " + (char) b );
+            b = inFromServer.read();
+            System.err.println(" " + (char) b );
+            return ERROR_SERVER_INVALID_REPLY;
+        }
+        b = inFromServer.read();
+        if(':' != b)
+        {
+            System.err.println("ERROR: reply invalid (5)!");
+            return ERROR_SERVER_INVALID_REPLY;
+        }
+        b = inFromServer.read();
+        if('\n' != b)
+        {
+            System.err.println("ERROR: reply invalid (6)!");
+            return ERROR_SERVER_INVALID_REPLY;
+        }
+        if("compare".equals(command))
+        {
+            if(true == identical)
+            {
+                return OK;
+            }
+            else
+            {
+                return NOT_IDENTICAL;
+            }
+        }
+        return OK;
+    }
+
+    private int executeCommand(String command, String remoteFileName)
+    {
+        boolean sendFile = false;
+        boolean receiveFile = false;
+        switch(command)
+        {
+        case "store":
+            sendFile = true;
+            break;
+
+        case "update":
+            sendFile = true;
+            break;
+
+        case "get":
+            receiveFile = true;
+            break;
+
+        case "has":
+            break;
+
+        case "compare":
+            receiveFile = true;
+            break;
+
+        default:
+            System.err.println("ERROR: invalid command!");
+            return ERROR_USER_INVALID_COMMAND;
+        }
+        try
+        {
+            int res = sendRequest(command, remoteFileName, sendFile);
+            if(0 != res)
+            {
+                // sending the request did not work
+                return res;
+            }
+            // Request has been send to the server
+
+            // Parse reply
+            int b = inFromServer.read();
+            if('2' != b)
+            {
+                System.err.println("ERROR: reply invalid (1)!");
+                return ERROR_SERVER_INVALID_REPLY;
+            }
+            b = inFromServer.read();
+            if(':' != b)
+            {
+                System.err.println("ERROR: reply invalid (2)!");
+                return ERROR_SERVER_INVALID_REPLY;
+            }
+            b = inFromServer.read();
+            if('0' != b)
+            {
+                System.out.println("Error state " + (char)b + " received from Server!");
+                int err = b;
+                StringBuffer Reason = new StringBuffer();
+                try
+                {
+                    do {
+                        b = inFromServer.read();
+                        Reason.append((char)b);
+                    }while('\n' != b);
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                    System.err.println("ERROR: IOException !");
+                }
+                System.out.println("Reason: " + Reason.toString());
+                return (Math.abs(err) * -1) + ERR_REPORTED_SERVER;
+            }
+            b = inFromServer.read();
+            if(':' != b)
+            {
+                System.err.println("ERROR: reply invalid (3)!");
+                return ERROR_SERVER_INVALID_REPLY;
+            }
+            StringBuffer DataSections = new StringBuffer();
+            do {
+                b = inFromServer.read();
+                DataSections.append((char)b);
+            }while('\n' != b);
+            String[] sections = DataSections.toString().split(":");
+            if(true == receiveFile)
+            {
+                res = handleReceiveFile(sections, command);
+            }
+            return res;
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            System.err.println("ERROR: IOException !");
+        }
+        System.err.println("ERROR: reply invalid (7)!");
+        return ERROR_SERVER_INVALID_REPLY;
+    }
+
+}
